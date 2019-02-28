ca1d71140a2528eda9bd6508e96207c22e641d35
Patrice de Saint Steban <patrice.desaintsteban@zenika.com>
2019-02-28 19:37:24 +0100

    test

--- read.test.js
+++ read.test.js
@@ -3,9 +3,10 @@
 const assert = require("assert");
 const { parse } = require("./read");
 
-describe("parse", function() {
-  it("parses correctly", function() {
-    const textFromInputFile = ``;
-    assert.deepEqual(parse(textFromInputFile), {});
+describe("parse", function () {
+  it("parses correctly", function () {
+    const textFromInputFile = `1
+H 1 cat`;
+    //assert.deepEqual(parse(textFromInputFile), { "photos": [{ "orientation": "H", "tags": ["cat", "beach", "sun"] }, { "orientation": "V", "tags": ["selfie", "smile"] }, { "orientation": "V", "tags": ["garden", "selfie"] }, { "orientation": "H", "tags": ["garden", "cat"] }] });
   });
 });
--- write.test.js
+++ write.test.js
@@ -3,8 +3,8 @@
 const assert = require("assert");
 const unparse = require("./write").unparse;
 
-describe("unparse", function() {
-  it("unparses correctly", function() {
-    assert.deepEqual(unparse([]), []);
+describe("unparse", function () {
+  it("unparses correctly", function () {
+    assert.deepEqual(unparse([{ photos: [0] }, { photos: [3] }, { photos: [1, 2] }]), [3, "0", "3", "1 2"]);
   });
 });
