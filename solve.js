42f0afbd654a14122bb241167df37a962224ce6b
Claveaum <maxime.claveau@zenika.com>
2019-02-28 19:56:58 +0100

    tayo!

--- solve.js
+++ solve.js
@@ -2,8 +2,34 @@ const debug = require("debug")("solve");
 const _ = require("lodash");
 const gridUtils = require("./grid-utils");
 
+function generateResult(acc, p, i) {
+  const index = i + 1;
+  const photo = p;
+  photo.index = index;
+  const resultSize = acc.length;
+  if (!resultSize) {
+    return [{ photos: [photo] }];
+  }
+  const lastResult = acc[resultSize - 1];
+  if (lastResult && lastResult.photos.length < 2) {
+    if (lastResult.photos[0].orientation === "H") {
+      return [...acc, { photos: [photo] }];
+    }
+  }
+  return acc;
+}
+
 function solve(problem, file) {
-  return [{ photos: [0] }, { photos: [3] }, { photos: [1, 2] }];
+  // destructure this!
+  const { photos } = problem;
+  const resultSorted = _.sortBy(photos, ["orientation"]);
+  const slidesComplete = resultSorted.reduce(
+    (acc, photo, i) => generateResult(acc, photo, i),
+    []
+  );
+  return slidesComplete.map(p => ({
+    photos: p.photos.map(photo => photo.index)
+  }));
 }
 
 module.exports = solve;
